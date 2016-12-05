d3f5bab2e5779f5cc52deb66e4c0db329f62df99
Maximiliano Guzenski <max@guzenski.com.br>
2016-12-05 20:39:47 -0300

    initial inferno test

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+import React, { Component } from 'react';
+import { BrowserRouter, Match, Miss, Redirect, Link } from 'react-router';
+import styled from 'styled-components'
+
+
+const P = styled.p`
+  margin-bottom: 25px;
+  color: ${props => props.color};
+`
+
+const Page1 = () => <h1>Page 1</h1>
+const Page2 = () => <h1>Page 2</h1>
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <P color='red'>This should be red</P>
+        <p>
+          <Link to='/'>page1</Link> - <Link to='/page2'>page2</Link>
+        </p>
+
+        <div>
+          <Match pattern="/" exactly component={Page1} />
+          <Match pattern="/page2" component={Page2} />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default () => (
+  <BrowserRouter><App /></BrowserRouter>
+);
