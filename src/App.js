1f8e23418a943a9ed0f2e26b3793020b44262da8
Maximiliano Guzenski <max@guzenski.com.br>
2016-12-08 12:02:00 -0300

    fix on react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react';
-import { BrowserRouter, Match, Link } from 'react-router';
+import { BrowserRouter, Match, Link, Redirect, Miss } from 'react-router';
 import styled from 'styled-components'
 
 
@@ -14,8 +14,14 @@ P.defaultProps = {
   color: 'green'
 }
 
-const Inner1 = <h3>inner1</h3>
-const Inner2 = <h3>inner1</h3>
+const Inner1 = ({text}) => (
+  <h3>inner1 - {text}</h3>
+)
+
+const Inner2 = ({text}) => (
+  <h3>inner2 - {text}</h3>
+)
+
 
 
 const Page1 = () => (
@@ -23,13 +29,11 @@ const Page1 = () => (
     <h1>Page1</h1>
 
     <p>
-      <Link to='/page1/inner1'>inner1</Link> - <Link to='/page1/inner2'>inner2</Link>
+      <Link to='/page1'>inner1</Link> - <Link to='/page1/inner2'>inner2</Link>
     </p>
 
-    <div>
-      <Match pattern="/page1/inner1" component={Inner1} />
-      <Match pattern="/page1/inner2" component={Inner2} />
-    </div>
+    <Match exactly pattern="/page1"        component={Inner1} />
+    <Match exactly pattern="/page1/inner2" component={Inner2} />
   </div>
 )
 
@@ -38,13 +42,11 @@ const Page2 = () => (
     <h1>Page2</h1>
 
     <p>
-      <Link to='/page2/inner1'>inner1</Link> - <Link to='/page2/inner2'>inner2</Link>
+      <Link to='/page2'>inner1</Link> - <Link to='/page2/inner2'>inner2</Link>
     </p>
 
-    <div>
-      <Match pattern="/page2/inner1" component={Inner1} />
-      <Match pattern="/page2/inner2" component={Inner2} />
-    </div>
+    <Match exactly pattern="/page2"        render={() => <Inner1 text='from page2 as render' />} />
+    <Match exactly pattern="/page2/inner2" render={() => <Inner2 text='from page2 as render' />} />
   </div>
 )
 
@@ -60,10 +62,9 @@ class App extends Component {
           <Link to='/'>page1</Link> - <Link to='/page2'>page2</Link>
         </p>
 
-        <div>
-          <Match pattern="/"      component={Page1} />
-          <Match pattern="/page2" component={Page2} />
-        </div>
+        <Match pattern="/page1" component={Page1} />
+        <Match pattern="/page2" component={Page2} />
+        <Miss  render={() => <Redirect to="/page1" />} />
       </div>
     );
   }
